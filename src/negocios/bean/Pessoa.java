283cc278676c8434b231cb21a5af25acfc783c5b
gabriel-ggpk <gabriel.cavalcanti.28@gmail.com>
2021-01-20 16:55:51 -0300

    Update Pessoa.java

--- src/negocios/bean/Pessoa.java
+++ src/negocios/bean/Pessoa.java
@@ -6,4 +6,26 @@ public abstract class Pessoa {
 		this.nome =nome;
 		this.senha = senha;
 	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (senha == null) {
+			if (other.senha != null)
+				return false;
+		} else if (!senha.equals(other.senha))
+			return false;
+		return true;
+	}
+	
 }
