37fbbd4849b704db76ee34264ba569300b1996d0
gabriel-ggpk <gabriel.cavalcanti.28@gmail.com>
2021-01-21 14:48:43 -0300

    Update AlunoMatriculado.java

--- src/negocios/bean/AlunoMatriculado.java
+++ src/negocios/bean/AlunoMatriculado.java
@@ -5,12 +5,13 @@ public class AlunoMatriculado {
  private float[] notas;
  private int faltas;
  private Disciplina disciplina;
- 
+ private boolean cursando;
 public AlunoMatriculado(Aluno aluno, Disciplina disciplina) {
 	this.aluno = aluno;
 	this.notas = new float[4];
 	this.faltas = 0;
 	this.disciplina = disciplina;
+	cursando = true;
 }
 
  
