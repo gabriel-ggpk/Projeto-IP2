705710ce42e9fd0728273ebd5f5f3f1b7c676cac
gustavoN002 <70725060+gustavoN002@users.noreply.github.com>
2021-01-25 20:00:16 -0300

    Update Coordenacao.java

--- src/negocios/bean/Coordenacao.java
+++ src/negocios/bean/Coordenacao.java
@@ -3,7 +3,7 @@ package negocios.bean;
 public class Coordenacao extends Pessoa {
 	private String departamento;
 
-	public Coordenacao(String nome, String senha,String codigo, String departamento) {
+	public Coordenacao(String nome, String senha, String codigo, String departamento) {
 		super(nome, senha,codigo);
 		this.departamento = departamento;
 	}
