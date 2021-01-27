a067801303233bf5971f7b4049b5e3fb75f5bb16
Luca Lemos <luca.mlemos@gmail.com>
2021-01-26 21:38:14 -0300

    atualização do main

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,18 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-	
-	@Override
-	public void start(Stage estagio1) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("teste.fxml"));
-		
-		estagio1.setTitle("Hello Woeld!");
-		estagio1.setScene(new Scene(root, 400, 300));
-		estagio1.show();
-	}
+public class Main {
 	
 	public static void main(String[] args) {
-		launch(args);
+		
 	}
 }
