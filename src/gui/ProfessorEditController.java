d26529f3942c973a1600027b537e0c42ffdb6cdf
Luca Lemos <luca.mlemos@gmail.com>
2021-02-02 01:47:49 -0300

    Professor notas

--- src/gui/ProfessorEditController.java
+++ src/gui/ProfessorEditController.java
@@ -3,9 +3,12 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import negocios.bean.AlunoMatriculado;
@@ -13,9 +16,13 @@ import negocios.bean.AlunoMatriculado;
 public class ProfessorEditController implements Initializable{
 	
 	AlunoMatriculado alunoSelecionado = null;
+	ObservableList<String> provas = FXCollections.observableArrayList();
 	
 	@FXML
 	private Label aluno;
+	
+	@FXML
+	private ChoiceBox<String> qualProva;
 	
 	@FXML
 	private Label erro;
@@ -26,8 +33,11 @@ public class ProfessorEditController implements Initializable{
 	@FXML
 	private void darNota(ActionEvent event) {
 		String strNota = escreverNota.getText();
+		String strProva = qualProva.getSelectionModel().getSelectedItem();
+		int intProva = 0;
 		double doubNota = -1;
 		try {
+			intProva = Integer.parseInt(strProva);
 			doubNota = Double.parseDouble(strNota);
 			erro.setText("");
 		}
@@ -36,7 +46,9 @@ public class ProfessorEditController implements Initializable{
 		}
 		finally {
 			if(doubNota >= 0 && doubNota <= 10) {
-				
+				alunoSelecionado.adicionarNota(doubNota, intProva);
+				aluno.setText(alunoSelecionado.toString());
+				System.out.println(intProva + " " + doubNota);
 			}
 			else {
 				erro.setText("Valor Invalido");
@@ -61,9 +73,18 @@ public class ProfessorEditController implements Initializable{
 		alunoSelecionado = matricula;
 		aluno.setText(alunoSelecionado.toString());
 	}
+	
+	public void settings() {
+		provas.add("1");
+		provas.add("2");
+		provas.add("3");
+		provas.add("4");
+		qualProva.setItems(provas);
+	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		settings();
 	}
 
 }
