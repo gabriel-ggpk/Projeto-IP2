a66d080f9b031114a12215395c1ac98abc2997c5
gabriel-ggpk <gabriel.cavalcanti.28@gmail.com>
2021-02-09 17:15:45 -0300

    json update

--- src/negocios/TesteJson.java
+++ src/negocios/TesteJson.java
@@ -7,40 +7,35 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import com.google.gson.*;
-
 import negocios.bean.Aluno;
 import negocios.bean.Pessoa;
 public class TesteJson {
-	
-
-	      /* public static void main(String[] args) throws IOException {
-	    	   ArrayList<Aluno> alunos = new ArrayList<>();
-	       Aluno teste = new Aluno("gabriel","senha","codigo");
-	       alunos.add(teste);
+		
 	       
-	       TesteJson testejson = new TesteJson();
-	     // testejson.writeAlunoJSON(teste);
-	      Aluno teste1 = testejson.readJSON("Json/teste.json");
-	      System.out.println(teste1);
-	       }*/
-	       
-	      void writeAlunoJSON(ArrayList<Pessoa> alunos) throws IOException { 
+	     public void writeAlunoJSON(ArrayList<Aluno> pessoas) throws IOException { 
+	 
+	    	
 	          GsonBuilder builder = new GsonBuilder(); 
-	          builder.setPrettyPrinting();
+	          //builder.setPrettyPrinting();
 	          Gson gson = builder.create(); 
 	          FileWriter writer = new FileWriter("Json/teste.json");   
-	          writer.write(gson.toJson(alunos));   
+	          writer.write(gson.toJson(pessoas));   
 	          writer.close(); 
 	       }  
 	       
-	       private Aluno readJSON(String arquivo) throws FileNotFoundException { 
+	       
+		public ArrayList<Pessoa> readJSON() throws FileNotFoundException { 
+	    	
 	          GsonBuilder builder = new GsonBuilder(); 
 	          Gson gson = builder.create(); 
-	          BufferedReader bufferedReader = new BufferedReader(
-	             new FileReader(arquivo));   
-	          
-	          Aluno aluno = gson.fromJson(bufferedReader,Aluno.class); 
-	          return aluno; 
+	          BufferedReader bufferedReader = new BufferedReader( new FileReader("Json/teste.json"));  
+	          JsonArray pessoas =  gson.fromJson(bufferedReader, JsonArray.class);
+	          for(JsonElement p: pessoas) {
+	        	  Aluno a = gson.fromJson(p, Aluno.class);
+	        	  System.out.println(a);
+	          }
+	          //System.out.println(pessoas);
+	          return null; 
 	       } 
 	       
 	}
